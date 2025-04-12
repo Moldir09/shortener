44fab94f8c84966be43d299b5ba534a660f3b3dc
m.kazhiyakhmetova <m.kazhiyakhmetova@small.kz>
2025-04-12 17:58:50 +0500

    iter4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,18 +4,22 @@ import (
 	"github.com/Moldir09/shortener.git/internal/app/handler"
 	"github.com/Moldir09/shortener.git/internal/app/service"
 	"github.com/Moldir09/shortener.git/internal/app/storage"
+	"github.com/Moldir09/shortener.git/internal/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	cfg := config.NewConfig()
+
 	storage := storage.NewInMemoryURLStore()
-	service := service.NewURLShortenerService(storage) // Создаем сервис
+	service := service.NewURLShortenerService(storage, cfg.BaseURL) // Создаем сервис
 	handler := handler.NewHandler(service)
 
 	r := gin.Default()
 
 	handler.RegisterRoutes(r)
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(cfg.ServerAddress); err != nil {
 		panic(err)
 	}
 
